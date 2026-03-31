615eadccc5e17fb3a6b1e9357cbcd5b560376dc9
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-03-31 20:20:36 +0530

    Add files via upload

---
+++ After class project/Seling your ride.py
@@ -0,0 +1,23 @@
+print("B1:Bike")
+print("B2:Car")
+num1=input("Enter Which ride do you want  ")
+if num1 == "B1" :
+    print("A1:Hero Splender+")
+    print("A2;Hero HF Deluxe")
+    num2=input("Enter your Bike type   ")
+if num2 == "A1" :
+
+    print("Your ride is on the way")
+if num2 == "A2":
+    print("Your ride is on the way")
+if num1 == "B2" :
+    print("A1:maruti wagonar")
+    print("A2;VW Polo")
+    num2=input("Enter your car type  ")
+if num2 == "A1" :
+    print("Your ride is on the way")
+if num2 == "A2" :
+    print("Your ride is on the way")
+
+else :
+    print("Adress Track ")

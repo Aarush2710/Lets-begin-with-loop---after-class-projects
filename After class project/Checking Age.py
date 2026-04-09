734d8a6f883c4bb81272ed50b0578e0434f703ae
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-04-09 22:02:25 +0530

    Add files via upload

---
+++ After class project/Checking Age.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your Age  "))
+if age > 18  :
+    print(age)
+    print("For sure,You are a adult,oldie")
+elif age == 18 :
+    print(age)
+    print("For sure,You are a adult,oldie")
+else:
+    print(age)
+    print("For sure,You are a child,young man")

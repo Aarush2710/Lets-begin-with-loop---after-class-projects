c525381c27f30b71b6ef2a6930c697051158b923
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-04-24 20:56:43 +0530

    Add files via upload

---
+++ After class project/Square.py
@@ -0,0 +1,11 @@
+import turtle
+screen=turtle.Screen()
+screen.setup(500,500)
+screen.title("Trinangle")
+screen.bgcolor("blue")
+turtle.done()
+board=turtle.Turtle()
+for i in range (3):
+    board.forward(100)
+    board.right(120)
+turtle.done()
